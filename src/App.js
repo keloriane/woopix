358f397bc76e7b9e5f7359ed48a7914bfc93cf4f
ksomao <somao@outlook.com>
2020-09-23 23:00:48 +0200

    global styles added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import Services from './pages/Services'
+import {createGlobalStyle,ThemeProvider} from "styled-components";
+
+
+const theme = {
+  main: "green"
+};
 
 function App() {
   return (
     <div className="App">
-   
+      <ThemeProvider theme={theme}>
+       <Services></Services>
+      </ThemeProvider>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/pages/Services/index.js
@@ -0,0 +1,42 @@
+import React,{useEffect} from 'react'
+import styled,{withTheme} from 'styled-components'
+
+ const Services = (props) => {
+    return (
+        <Container> 
+
+        </Container>
+    )
+
+}
+
+export default withTheme(Services) 
+
+
+const Container = styled.div`
+height:100%;
+width:100%;
+font-size: 1.5em;
+text-align: center;
+background-color: red; 
+max-width: 1025px;
+
+    
+@media (min-width: 576px) {  
+}
+ 
+/* Medium devices (tablets, 768px and up) The navbar toggle appears at this breakpoint */
+@media (min-width: 768px) {  
+
+}
+ 
+/* Large devices (desktops, 992px and up) */
+@media (min-width: 992px) { 
+
+}
+ 
+/* Extra large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) {  
+}
+`;
+
