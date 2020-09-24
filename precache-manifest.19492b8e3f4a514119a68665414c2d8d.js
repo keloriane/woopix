9abfb4dd2901af0bd2dfe44013b6b68e51e79e29
keloriane <kevin.flbt@gmail.com>
2020-09-24 16:37:30 +0200

    Updates

---
+++ precache-manifest.19492b8e3f4a514119a68665414c2d8d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c339975d0c01fc7cd57355195a63ab1d",
+    "url": "/react-woopix/index.html"
+  },
+  {
+    "revision": "4eef6e1dfd9bbe476823",
+    "url": "/react-woopix/static/css/main.78cf3957.chunk.css"
+  },
+  {
+    "revision": "73ac91a4757d7b18391f",
+    "url": "/react-woopix/static/js/2.90528c51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-woopix/static/js/2.90528c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eef6e1dfd9bbe476823",
+    "url": "/react-woopix/static/js/main.03fcc975.chunk.js"
+  },
+  {
+    "revision": "12a338fbab6b77e8bf2f",
+    "url": "/react-woopix/static/js/runtime-main.86f25452.js"
+  },
+  {
+    "revision": "df6533c8383f2952f578e41a86a56632",
+    "url": "/react-woopix/static/media/home.df6533c8.png"
+  },
+  {
+    "revision": "376886039767b8d8014a00658867e582",
+    "url": "/react-woopix/static/media/slide1.37688603.png"
+  },
+  {
+    "revision": "6bb4d866ab0dcbdfc768636101a1ef65",
+    "url": "/react-woopix/static/media/slide2.6bb4d866.png"
+  },
+  {
+    "revision": "6979465045bb3bc3c53ace5a86f516df",
+    "url": "/react-woopix/static/media/slide3.69794650.png"
+  }
+]);
