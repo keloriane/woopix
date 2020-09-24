1a62cfe3aa57fc4feba74f5529cef0496dc8fde5
keloriane <kevin.flbt@gmail.com>
2020-09-24 10:32:36 +0200

    home-mobile view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Home } from './pages/Home/index.js';
+import { About } from './pages/About/index.js';
+import { BrowserRouter as HashRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <HashRouter>
+      <Switch>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </div>
+
+      </Switch>
+
+    </HashRouter>
   );
 }
 
